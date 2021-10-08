1afaae4d05fa90865769d42bcac0567da7ed4624
dopebear23 <palashjain2312000@gmail.com>
2021-10-09 00:48:21 +0530

    set required prop return value to boolean

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,57 +1,54 @@
 import mongoose from 'mongoose';
 
 export interface UserType {
-	id: string;
-	email: string;
-	name: string;
-	authType: 'google' | 'local';
-	hashedPassword?: string;
-	googleId?: string;
-	displayPicture?: string;
-	isAdmin?: boolean;
+    id: string;
+    email: string;
+    name: string;
+    authType: 'google' | 'local';
+    hashedPassword?: string;
+    googleId?: string;
+    displayPicture?: string;
+    isAdmin?: boolean;
 }
 
 const userSchema = new mongoose.Schema<UserType>({
-	email: {
-		type: String,
-		required: true,
-	},
-	authType: {
-		type: String,
-		required: true,
-	},
-	hashedPassword: {
-		type: String,
-		//@ts-ignore
-		required: function () {
-			//@ts-ignore
-			return this.authType === 'local';
-		},
-	},
-	googleId: {
-		type: String,
-		//@ts-ignore
-		required: function () {
-			//@ts-ignore
-			return this.authType === 'google';
-		},
-	},
-	name: {
-		type: String,
-		required: true,
-	},
-	displayPicture: {
-		type: String,
-		//@ts-ignore
-		required: function () {
-			//@ts-ignore
-			return this.authType === 'google';
-		},
-	},
-	isAdmin: {
-		type: Boolean,
-		required: false,
-	},
+    email: {
+        type: String,
+        required: true,
+    },
+    authType: {
+        type: String,
+        required: true,
+    },
+    hashedPassword: {
+        type: String,
+        required: function (): Boolean {
+            //@ts-ignore
+            return this.authType === 'local';
+        },
+    },
+    googleId: {
+        type: String,
+        required: function (): Boolean {
+            //@ts-ignore
+            return this.authType === 'google';
+        },
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    displayPicture: {
+        type: String,
+        required: function (): Boolean {
+            //@ts-ignore
+            return this.authType === 'google';
+        },
+    },
+    isAdmin: {
+        type: Boolean,
+        required: false,
+    },
 });
 
 export const User = mongoose.model<UserType>('User', userSchema);
