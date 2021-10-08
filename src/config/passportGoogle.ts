81ea461222f5ef1978a146be1788e583961aabed
dopebear23 <palashjain2312000@gmail.com>
2021-10-09 00:33:27 +0530

    change callback URL
    Signed-off-by: dopebear23 <palashjain2312000@gmail.com>

--- src/config/passportGoogle.ts
+++ src/config/passportGoogle.ts
@@ -8,43 +8,43 @@ dotenv.config();
 const GoogleStrategy = passportGoogle.Strategy;
 
 passport.use(
-	new GoogleStrategy(
-		{
-			clientID: process.env.GOOGLE_CLIENT_ID!,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-			callbackURL: '/auth/google/callback',
-		},
-		async function (accessToken, refreshToken, profile, done) {
-			try {
-				let user = await User.findOne({ googleId: profile.id });
-				if (user) done(null, user);
-				else {
-					const newUser = new User({
-						authType: 'google',
-						googleId: profile.id,
-						email: profile.emails![0].value,
-						name: profile.displayName,
-						displayPicture: profile.photos![0].value,
-					});
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            callbackURL: '/api/auth/google/callback',
+        },
+        async function (accessToken, refreshToken, profile, done) {
+            try {
+                let user = await User.findOne({ googleId: profile.id });
+                if (user) done(null, user);
+                else {
+                    const newUser = new User({
+                        authType: 'google',
+                        googleId: profile.id,
+                        email: profile.emails![0].value,
+                        name: profile.displayName,
+                        displayPicture: profile.photos![0].value,
+                    });
 
-					await newUser.save();
+                    await newUser.save();
 
-					done(null, newUser);
-				}
-			} catch (ex: any) {
-				done(ex);
-			}
-		}
-	)
+                    done(null, newUser);
+                }
+            } catch (ex: any) {
+                done(ex);
+            }
+        }
+    )
 );
 
 //@ts-ignore
 passport.serializeUser(function (user: UserType, done) {
-	done(null, user.id);
+    done(null, user.id);
 });
 
 passport.deserializeUser(function (id, done) {
-	User.findById(id, function (err: any, user: UserType) {
-		done(err, user);
-	});
+    User.findById(id, function (err: any, user: UserType) {
+        done(err, user);
+    });
 });
